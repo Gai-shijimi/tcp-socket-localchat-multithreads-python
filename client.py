50df631d089a015f6993ea68d2015c4b75d05656
Gai-shijimi <g.t.avenue@icloud.com>
2025-09-27 13:49:18 +0900

    added new code: server.py, client.py

--- client.py
+++ client.py
@@ -1,4 +1,44 @@
 import socket
 import signal
 import sys
+import threading
 
+def handler(signum, _):
+    print(f"{signum}を受信しました。終了しました。")
+    sys.exit(0)
+
+def handle_message(file):
+    try:
+        while True:
+            message = input('message >>>> ')
+            file.write((message + '\n').encode('utf-8'))
+
+            data = file.readline()
+            print('サーバーからの応答：', data.decode('utf-8').rstrip('\n'))
+
+            if not data:
+                print('サーバーが切断しました。')
+                break
+    finally:
+        print('ソケットを閉じます。')
+        sock.close()
+
+
+server_address = '/tmp/socket_file'
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+try:
+    sock.connect(server_address)
+except socket.error as err:
+    print(err)
+    sys.exit(1)
+
+
+file = sock.makefile('rwb', buffering=0)
+
+signal.signal(signal.SIGINT, handler)
+
+
+thread_handle_message = threading.Thread(target=handle_message, args=(file,), daemon=True)
+thread_handle_message.start()
--- server.py
+++ server.py
@@ -2,11 +2,21 @@ import socket
 import os
 import signal
 import sys 
+import threading
 
 def handler(signum, _):
     print(f'{signum}を受け取りました。')
     sys.exit(0)
 
+def handle_client(connection, ):
+    print("クライアントと接続しました。")
+
+    file = connection.makefile('rwb', buffering=0)
+    for line in file:
+        msg = line.rstrip(b'\n').decode('utf-8')
+        print('受信：', msg)
+        response = ('サーバーからの応答：' + msg + '\n').encode('utf-8')
+        file.write(response)
 
 server_address = '/tmp/socket_file'
 
@@ -22,4 +32,11 @@ sock.bind(server_address)
 sock.listen(1)
 
 signal.signal(signal.SIGINT, handler)
+
+while True:
+    connection, _ = sock.accept()
+
+    thread_handle_client = threading.Thread(target=handle_client, args=(connection,), daemon=True)
+    thread_handle_client.start()
+
 
